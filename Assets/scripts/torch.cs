6567ba33853d15b92bfdddc8285cc53f7840bd91
meyou7677 <147900758+meyou7677@users.noreply.github.com>
2025-03-29 08:59:14 +0800

    g

--- Assets/scripts/torch.cs
+++ Assets/scripts/torch.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class torch : MonoBehaviour
 {
     Light m_light;
-    public float battery;
+    public float battery_energy;
+    public float min_energy = 0;
+    public float max_energy = 30;
+    public Slider battery_slider;
+    public float dimSpeed = 1.0f;
     
     // Start is called before the first frame update
     void Start()
     {
         m_light = GetComponent<Light>();
+        battery_energy = max_energy;
+        battery_slider.minValue = min_energy;
+        battery_slider.maxValue = max_energy;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_light.intensity -= battery * Time.deltaTime;
+        battery_energy -= dimSpeed * Time.deltaTime;
+        m_light.intensity = battery_energy;
+        battery_slider.value = battery_energy;
     }
 }
