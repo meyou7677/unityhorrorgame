671f2a36da7e05af87c68562daf9e7ccf155991f
meyou7677 <147900758+meyou7677@users.noreply.github.com>
2025-04-09 18:57:34 +0800

    enemyfollow

--- Assets/scripts/wolfenemy.cs
+++ Assets/scripts/wolfenemy.cs
@@ -6,22 +6,47 @@ public class wolfenemy : MonoBehaviour
 {
     Animator animator;
     Rigidbody rb;
+    public float walking_speed;
+    public float running_speed;
+    private GameObject myplayer;
+    public float attackD;
+
     
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        myplayer = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I)) 
+        Vector3 direction = myplayer.transform.position - transform.position;
+        Vector3 newdirection = new Vector3(direction.x, 0, direction.z);
+        if (newdirection.magnitude > attackD) 
         {
+            Vector3 target = transform.position + newdirection;
+            transform.LookAt(target);
+            rb.AddForce(newdirection.normalized, ForceMode.Impulse);
+            SpeedControl();
+
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
             animator.SetTrigger("attack");
         }
-        rb.AddForce(new Vector3 (1,0,0), ForceMode.Impulse);
+        
 
+    }
+    private void SpeedControl()
+    {
+        var velocity = rb.velocity;
+        var flatVelocity = new Vector3(velocity.x, 0f, velocity.z);
+        if (!(flatVelocity.magnitude > walking_speed)) return;
+        var limitedVelocity = flatVelocity.normalized * walking_speed;
+        rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
     }
 }
